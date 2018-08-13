0ea08ab83e71fde0eb329a5ac3281a1eb9943796
cmonkey03 <russellpbrown@gmail.com>
2018-08-12 23:53:06 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,16 +10,21 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
 
-  def post_params
-    params.permit(:category, :content, :title)
-  end
+    def post_params
+      params.permit(:category, :content, :title)
+    end
 
-  def set_post!
-    @post = Post.find(params[:id])
-  end
+    def set_post!
+      @post = Post.find(params[:id])
+    end
 end
